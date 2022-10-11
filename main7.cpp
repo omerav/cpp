0ce0ccbcc4131c5c4a11c877e9ee04c5c2a8fc93
Omer_A <>
2022-10-11 19:03:19 +0300

    modified:   main7.cpp 	new file:   main7.exe 	new file:   main8.cpp

--- main7.cpp
+++ main7.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 
-
-main() {
+main()
+{
     using namespace std;
 
+    char character1{'a'};
+    char character2{'r'};
+    char character3{'r'};
+    char character4{'r'};
+    char character5{'o'};
 
+    cout << character1 << endl;
+    cout << character2 << endl;
+    cout << character3 << endl;
+    cout << character4 << endl;
+    cout << character5 << endl;
 
+    // One byte in memory : 2^8 = 256 different values (0 ~ 255)
+    cout << endl;
 
+    char value = 65;                     // ASCII character code for 'A'
+    cout << "value : " << value << endl; // A
+    cout << "value(int) : " << static_cast<int>(value) << endl;
 
-    return 0;    
+    return 0;
 }
---
+++ main8.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+main()
+{
+    using namespace std;
+
+
+
+    return 0;
+}
